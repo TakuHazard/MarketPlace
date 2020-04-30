15e232753371ba76a8d4cd361cb3bbd55c838734
Cesar Moreno <cmoreno@sfu.ca>
2020-04-30 12:28:23 -0700

    Add a button for new listing.

--- app/src/main/java/videodemos/example/marketplace/ListingActivity.kt
+++ app/src/main/java/videodemos/example/marketplace/ListingActivity.kt
@@ -2,11 +2,23 @@ package videodemos.example.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class ListingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
+
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        val addNewListing : Button = findViewById(R.id.btn_post_new_listing)
+        addNewListing.setOnClickListener{
+            Toast.makeText(this, "Button Clicked!", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
