8676c83b65fa2835d2996d7eaeed969678a8c750
Karen Chen <karencyuan@gmail.com>
2020-05-01 22:11:53 -0700

    Switch to dashboard once logged in or registered.

--- app/src/main/java/videodemos/example/marketplace/RegisterActivity.kt
+++ app/src/main/java/videodemos/example/marketplace/RegisterActivity.kt
@@ -105,6 +105,10 @@ class RegisterActivity : AppCompatActivity() {
                     }
 
                 Log.d("RegisterActivity", "done")
+
+                val intent = Intent(this, DashboardActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
             }
     }
 }
